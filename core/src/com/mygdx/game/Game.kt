526ba98d551cd591166a416c147c36f2f384534d
Roman <hmrfins@gmail.com>
2019-12-04 20:17:28 +0300

    more playable with enlarget constans :)

--- core/src/com/mygdx/game/Game.kt
+++ core/src/com/mygdx/game/Game.kt
@@ -2,16 +2,6 @@ package com.mygdx.game
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.Sprite
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer
-import com.badlogic.gdx.math.Vector2
-import com.badlogic.gdx.physics.box2d.*
-import com.mygdx.game.entities.Player
-import com.mygdx.game.entities.Ring
 import com.mygdx.game.screens.PlayScreen
 
 class Game : ApplicationAdapter() {
