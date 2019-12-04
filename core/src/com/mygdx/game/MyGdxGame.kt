4712e470ca36a23353889bd81168fd9c995d31fb
labtorie <56982278+labtorie@users.noreply.github.com>
2019-12-04 04:41:00 +0300

    zhopa

--- core/src/com/mygdx/game/MyGdxGame.kt
+++ core/src/com/mygdx/game/MyGdxGame.kt
@@ -5,8 +5,16 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Animation
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.*
+import com.badlogic.gdx.utils.Array
+import kotlin.math.PI
 
 
 class MyGdxGame : ApplicationAdapter() {
@@ -14,7 +22,14 @@ class MyGdxGame : ApplicationAdapter() {
     lateinit var debugRenderer: Box2DDebugRenderer
     lateinit var camera: OrthographicCamera
     lateinit var body: Body
-
+    lateinit var batch: SpriteBatch
+    lateinit var texture: Texture
+    lateinit var ringTexture: Texture
+    lateinit var sprite: Sprite
+    lateinit var ringSprite: Sprite
+    lateinit var shapeRenderer: ShapeRenderer
+    lateinit var anim: Animation<TextureRegion>
+    var stateTime = 0f
     // Creating the box which represents the fighters' behavior
     private fun createPlayer(world: World, x: Float, y: Float) {
         val rectDef = BodyDef()
@@ -36,6 +51,8 @@ class MyGdxGame : ApplicationAdapter() {
         rectFixDef.restitution = 0.3f
         rectFixDef.friction = 1f
 
+
+
         body = world.createBody(rectDef)
         body.createFixture(rectFixDef)
         rectShape.dispose()
@@ -45,11 +62,33 @@ class MyGdxGame : ApplicationAdapter() {
     override fun create() {
         Box2D.init()
         camera = OrthographicCamera(Gdx.graphics.width.toFloat() / cameraScale, Gdx.graphics.height.toFloat() / cameraScale)
-        debugRenderer = Box2DDebugRenderer(true, true, false, true, true, true)
+        debugRenderer = Box2DDebugRenderer(true, false, false, false, false, false)
         createPlayer(world, 0f, 0f)
 
+        batch = SpriteBatch()
+        texture = Texture("player.png")
+        val split = TextureRegion.split(texture, texture.width / 8, texture.height)
+      //  val frames = Array<TextureRegion>().apply {
+      //      for (i in 0 until size) add(split[0][i])
+      //  }
+
+        val frames = Array<TextureRegion>()
+        for (i in 0..7)
+        {
+            frames.add(split[0][i])
+        }
+        anim = Animation<TextureRegion>(0.1f, frames)
+
+        ringTexture = Texture("bkg.png")
+
+        sprite = Sprite(texture)
+        ringSprite = Sprite(ringTexture)
+
+        shapeRenderer = ShapeRenderer()
+        stateTime = 0f
 
     }
+
 
     private fun pushBody(dir: Int) {
         lateinit var impulseRaw: Vector2
@@ -72,6 +111,7 @@ class MyGdxGame : ApplicationAdapter() {
             impulseRaw = Vector2(sideForce, -forwardForce)
             pointRaw = Vector2(body.position.x + playerWidth, body.position.y + playerHeight)
         }
+
         body.applyLinearImpulse(impulseRaw.rotateRad(body.angle), pointRaw.rotateAroundRad(body.position, body.angle), true)
     }
 
@@ -92,11 +132,29 @@ class MyGdxGame : ApplicationAdapter() {
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
             pushBody(3)
         }
-        debugRenderer.render(world, camera.combined)
+           camera.position.set(Vector2(body.position.x /1.5f, body.position.y /1.5f), camera.position.z)
+
+        stateTime += Gdx.graphics.deltaTime
+
+      batch.projectionMatrix = camera.combined
+        var frame = anim.getKeyFrame(stateTime, true)
+        if (body.linearVelocity.len() < 2f && (body.angularVelocity < 1f && body.angularVelocity > -1f))
+        {
+            frame = anim.getKeyFrame(0f)
+        }
+
+
+        batch.begin()
+
+        batch.draw(ringSprite,-7f, -7f, 14f, 14f)
+        batch.draw(frame, body.position.x -2f, body.position.y-2f, 2f, 2f, 4f, 4f, 1f, 1f, body.angle / PI.toFloat() * 180f)
+        batch.end()
+        camera.update()
         world.step(1 / targetFPS, 6, 2)
     }
 
     override fun dispose() {
-
+        batch.dispose()
+        texture.dispose()
     }
 }
---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.kt
@@ -0,0 +1,16 @@
+package com.mygdx.game.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.mygdx.game.MyGdxGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        config.height = 720
+        config.width = 342
+        LwjglApplication(MyGdxGame(), config)
+
+    }
+}
